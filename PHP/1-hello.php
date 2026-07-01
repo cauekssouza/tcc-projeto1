c6e51a6265f527be6353640881ff971ae5e4a666
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2026-07-01 16:14:11 -0300

    Delete PHP directory

--- PHP/1-hello.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE>  
-<html>  
-<body> 
-<?php  
-echo "<h2>Hello by PHP</h2><br>"; 
-echo "<h2>Hello by PHP</h2>"; 
-?>  
-</body>  
-</html>  
--- PHP/13-session-II.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE>  
-<html>  
-<body>  
-<?php  
-session_start();
-echo $_SESSION['favanimal']."<br>"; 
-?>  
-</body>  
-</html>  
